0a6f8d22f0d89e1ca1206af740c6dd73d707063a
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-16 17:58:22 -0600

    Import remaining data file and add basic NewsContainer tag

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology
     }
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <h1>Da Newz</h1>
+        <NewsContainer />
       </div>
     );
   }
