352bdbe13e0c5aaca8cc9dbc1a1199df4deb0fcb
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-16 18:22:53 -0600

    Create basic map structure for news articles

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import NewsArticle from '../NewsArticle/NewsArticle.js'
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({ articles }) => {
+  const newsArticles = articles.map(article => (
+    <NewsArticle
+
+    />
+  ))
+
+  return <div  className="articles-container">{newsArticles}</div>
+}
 
 export default NewsContainer;
