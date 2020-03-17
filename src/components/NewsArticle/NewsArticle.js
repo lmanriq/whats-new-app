d1e32f7308f6063b1f39f1c3d277aeef9e7065fa
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-16 18:22:15 -0600

    Create general structure of the news article html article element using props

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = (props) => {
+  return (
+    <article className="article">
+      <h3>{props.headline}</h3>
+      <a href={props.url}><img src={props.img} alt="article photo"></img></a>
+      <h4>{props.description}</h4>
+    </article>
+  )
+};
 
 export default NewsArticle;
