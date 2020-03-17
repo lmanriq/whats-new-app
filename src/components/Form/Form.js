178c10703acde152ef0e6ac60b5e7f8814007768
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-17 17:39:24 -0600

    Add updateState method and form fields

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,16 +5,31 @@ class Form extends Component {
   constructor(props) {
     super();
     this.state = {
-      query: null
+      search: ''
     }
+  }
+
+  updateState = (e) => {
+    this.setState({search: e.target.value})
   }
 
   render() {
     return (
-      
+      <form>
+        <input 
+          title="search"
+          type="text"
+          placeholder="Search Articles"
+          value={this.state.search}
+          onChange={this.updateState}
+        />
+        <button
+          onClick={}>
+          <img src={require(`../../icons/search-icon.svg`)} alt="search icon"/> 
+        </button>
+      </form>
     )
   }
-
 };
 
 export default Form;
