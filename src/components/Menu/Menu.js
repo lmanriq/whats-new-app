ea08bf342a0871b27a625d709a9c3e35c58f076c
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-17 17:20:09 -0600

    Add nav icons and style nav bar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
 import './Menu.css'
 
-class Menu extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      topic: null
-    }
-  }
-
-  render() {
-    const buttons = Object.keys(this.props.topics).map(topic => (
-      <button key={this.props.topics[topic].key}
-              onClick={() => this.props.filterTopic(topic)}>
-              {topic}</button>
-    ))
+const Menu = (props) => {
+    const buttons = Object.keys(props.topics).map(topic => {
+      const imgUrl = require(`../../icons/${topic}.svg`);
+      return (
+        <button key={props.topics[topic].key}
+                onClick={() => props.filterTopic(topic)}>
+                <img src={imgUrl} alt={topic + ' icon'}/>
+                {topic}</button>
+      )
+    })
 
     return (
       <nav>
         {buttons}
       </nav>
     )
-  }
 }
 
 export default Menu;
