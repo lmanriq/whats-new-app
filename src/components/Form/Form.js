6c9212357fd35248bb052a61c360aa451d6f482c
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-17 17:29:46 -0600

    Add Form files and outline Form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      query: null
+    }
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+
+};
+
+export default Form;
