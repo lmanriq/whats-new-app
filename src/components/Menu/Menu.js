f530632c13993e5709adf3d7c7a96c98535af92f
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-16 21:41:10 -0600

    Create nav buttons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css'
 
+class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isCool: true
+    }
+  }
+
+  render() {
+    const labels = Object.keys(this.props).map(prop => (
+      <button key={this.props[prop].key}>{prop}</button>
+    ))
+
+    return (
+      <nav>
+        {labels}
+      </nav>
+    )
+  }
+
+  filterTopics() {
+
+  }
+}
 
 export default Menu;
