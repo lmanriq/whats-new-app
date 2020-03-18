3bf0d3ab60a2003a18b7bfcec84bb64e64af67c7
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-18 11:39:52 -0600

    Highlight buttons when selected

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css'
 
-const Menu = (props) => {
-    const buttons = Object.keys(props.topics).map(topic => {
-      const imgUrl = require(`../../icons/${topic}.svg`);
-      return (
-        <button key={props.topics[topic].key}
-                onClick={() => props.filterTopic(topic)}>
-                <img src={imgUrl} alt={topic + ' icon'}/>
-                {topic}</button>
-      )
-    })
 
-    return (
-      <nav>
-        {buttons}
-      </nav>
-    )
+class Menu extends Component {
+    constructor(props) {
+      super();
+      this.state = {
+        activeTopic: 'local'
+      }
+    }
+
+    changeActiveTopic(topic) {
+      this.props.filterTopic(topic);
+      this.setState({activeTopic: topic});
+    }
+
+    render() {
+      const buttons = Object.keys(this.props.topics).map(topic => {
+        const imgUrl = require(`../../icons/${topic}.svg`);
+        return (
+          <button key={this.props.topics[topic].key}
+                  onClick={() => this.changeActiveTopic(topic)}
+                  className={this.state.activeTopic === topic ? 'active' : ''}>
+                  <img src={imgUrl} alt={topic + ' icon'}/>
+                  {topic}</button>
+        )
+      })
+      return (
+        <nav>
+          {buttons}
+        </nav>
+      )
+    }
 }
+
+// const Menu = (props) => {
+//     const buttons = Object.keys(props.topics).map(topic => {
+//       const imgUrl = require(`../../icons/${topic}.svg`);
+//       return (
+//         <button key={props.topics[topic].key}
+//                 onClick={() => props.filterTopic(topic)}>
+//                 <img src={imgUrl} alt={topic + ' icon'}/>
+//                 {topic}</button>
+//       )
+//     })
+
+//     return (
+//       <nav>
+//         {buttons}
+//       </nav>
+//     )
+// }
 
 export default Menu;
