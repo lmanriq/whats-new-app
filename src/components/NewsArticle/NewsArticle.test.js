65500ba624b6e886c092d723e41923ad853be4ee
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-18 20:51:41 -0600

    Add NewsArticle test props

--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -8,7 +8,12 @@ describe ('NewsArticle', () => {
     // Setup
     // Render the component and pass it props that it needs
     const { getByText } = render(<NewsArticle 
-      
+        id={5}
+        key={5}
+        headline={'Florida Man Gets Eaten By Gators'}
+        img={'https://www.rollingstone.com/scary-gator-photo'}
+        description={'Gary "the Gator Man" Gorbichov gets ruthlesslessy mauled to death by King K. Rool and his cronies'}
+        url={'https://www.rollingstone.com/scary-gator-story'}
       />)
     // const labelEl = getByText('Search...');
     // expect(labelEl).toBeInTheDocument();
