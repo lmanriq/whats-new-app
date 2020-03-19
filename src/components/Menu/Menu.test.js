c89d3acdab28f5db1a53dec3c5237a76d3732f88
Lili Manrique <liliana.manrique1@gmail.com>
2020-03-19 15:04:00 -0600

    Add tests for menu rendering and filtering functionality

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -1,16 +1,77 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Menu from './Menu';
 import '@testing-library/jest-dom'
+import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 
 describe ('Menu', () => {
   it('renders text that we expect', () => {
-    // Setup
-    // Render the component and pass it props that it needs
+    const mockFilterTopic = jest.fn();
     const { getByText } = render(<Menu 
-      
+        topics = {
+          {
+            local: {
+              contents: local,
+              key: 1
+            },
+            entertainment: {
+              contents: entertainment,
+              key: 2
+            },
+            health: {
+              contents: health,
+              key: 3
+            },
+            science: {
+              contents: science,
+              key: 4
+            },
+            technology: {
+              contents: technology,
+              key: 5
+            }
+          }
+        }
+        filterTopic = {mockFilterTopic}
       />)
-    // const labelEl = getByText('Search...');
-    // expect(labelEl).toBeInTheDocument();
+    const scienceBtn = getByText('science');
+    expect(scienceBtn).toBeInTheDocument();
+  })
+
+  it('filters by topic', () => {
+    const mockFilterTopic = jest.fn();
+    const { getByText } = render(<Menu 
+        topics = {
+          {
+            local: {
+              contents: local,
+              key: 1
+            },
+            entertainment: {
+              contents: entertainment,
+              key: 2
+            },
+            health: {
+              contents: health,
+              key: 3
+            },
+            science: {
+              contents: science,
+              key: 4
+            },
+            technology: {
+              contents: technology,
+              key: 5
+            }
+          }
+        }
+        filterTopic = {mockFilterTopic}
+      />)
+      fireEvent.click(getByText('entertainment'));
+      expect(mockFilterTopic).toHaveBeenCalledWith('entertainment');
   })
 })
